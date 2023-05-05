c577d9534a54af5d0b620fdb7d3aaeeec3ac8aff
Braden Rogers <brogers42@my.bcit.ca>
2023-05-05 09:48:27 -0700

    Sprint 2 Deliverable (Updated With Comments)

--- chess.js
+++ chess.js
@@ -1,36 +1,9 @@
-// VARIABLES
-
 // Define size of a chessboard
 const boardSize = 8;
 // Create board variable
 var board;
-// Set the format for a square with no piece
+// Define the format for a square with no piece
 var nonePiece = ["empty.png", "notPiece"];
-
-// Formatting = array of pieces starting from left to right
-// image, selected, position, piece, status
-var whitePieces = [["wrook.png", "null", "a8", "rook", "on board"], ["wknight.png", "null", "b8", "knight", "on board"],
-["wbishop.png", "null", "c8", "bishop", "on board"], ["wqueen.png", "null", "d8", "queen", "on board"],
-["wking.png", "null", "e8", "king", "on board"], ["wbishop.png", "null", "f8", "bishop", "on board"],
-["wknight.png", "null", "g8", "knight", "on board"], ["wrook.png", "null", "h8", "rook", "on board"]];
-
-var blackPieces = [["brook.png", "null", "a8", "rook", "on board"], ["bknight.png", "null", "b8", "knight", "on board"],
-["bbishop.png", "null", "c8", "bishop", "on board"], ["bqueen.png", "null", "d8", "queen", "on board"],
-["bking.png", "null", "e8", "king", "on board"], ["bbishop.png", "null", "f8", "bishop", "on board"],
-["bknight.png", "null", "g8", "knight", "on board"], ["brook.png", "null", "h8", "rook", "on board"]];
-
-
-// Formatting = array of pawns starting from left to right
-// image, selected, position, promoted to, status
-var blackPawns = [["bpawn.png", "null", "a7", "pawn", "on board"], ["bpawn.png", "null", "b7", "pawn", "on board"],
-["bpawn.png", "null", "c7", "pawn", "on board"], ["bpawn.png", "null", "d7", "pawn", "on board"],
-["bpawn.png", "null", "e7", "pawn", "on board"], ["bpawn.png", "null", "f7", "pawn", "on board"],
-["bpawn.png", "null", "g7", "pawn", "on board"], ["bpawn.png", "null", "h7", "pawn", "on board"],];
-
-var whitePawns = [["wpawn.png", "null", "a2", "null", "on board"], ["wpawn.png", "null", "b2", "null", "on board"],
-["wpawn.png", "null", "c2", "pawn", "on board"], ["wpawn.png", "null", "d2", "pawn", "on board"],
-["wpawn.png", "null", "e2", "pawn", "on board"], ["wpawn.png", "null", "f2", "pawn", "on board"],
-["wpawn.png", "null", "g2", "pawn", "on board"], ["wpawn.png", "null", "h2", "pawn", "on board"]];
 
 // Array row and column of selected piece
 var selectedPieceRow;
@@ -44,8 +17,28 @@ var deletedPieces = 0;
 
 // Working
 function resetBoard() {
+    // Set all pieces to original states
+    var whitePieces = [["wrook.png", "null", "a8", "rook", "on board"], ["wknight.png", "null", "b8", "knight", "on board"],
+    ["wbishop.png", "null", "c8", "bishop", "on board"], ["wqueen.png", "null", "d8", "queen", "on board"],
+    ["wking.png", "null", "e8", "king", "on board"], ["wbishop.png", "null", "f8", "bishop", "on board"],
+    ["wknight.png", "null", "g8", "knight", "on board"], ["wrook.png", "null", "h8", "rook", "on board"]];
+
+    var blackPieces = [["brook.png", "null", "a8", "rook", "on board"], ["bknight.png", "null", "b8", "knight", "on board"],
+    ["bbishop.png", "null", "c8", "bishop", "on board"], ["bqueen.png", "null", "d8", "queen", "on board"],
+    ["bking.png", "null", "e8", "king", "on board"], ["bbishop.png", "null", "f8", "bishop", "on board"],
+    ["bknight.png", "null", "g8", "knight", "on board"], ["brook.png", "null", "h8", "rook", "on board"]];
+
+    var blackPawns = [["bpawn.png", "null", "a7", "pawn", "on board"], ["bpawn.png", "null", "b7", "pawn", "on board"],
+    ["bpawn.png", "null", "c7", "pawn", "on board"], ["bpawn.png", "null", "d7", "pawn", "on board"],
+    ["bpawn.png", "null", "e7", "pawn", "on board"], ["bpawn.png", "null", "f7", "pawn", "on board"],
+    ["bpawn.png", "null", "g7", "pawn", "on board"], ["bpawn.png", "null", "h7", "pawn", "on board"],];
+
+    var whitePawns = [["wpawn.png", "null", "a2", "null", "on board"], ["wpawn.png", "null", "b2", "null", "on board"],
+    ["wpawn.png", "null", "c2", "pawn", "on board"], ["wpawn.png", "null", "d2", "pawn", "on board"],
+    ["wpawn.png", "null", "e2", "pawn", "on board"], ["wpawn.png", "null", "f2", "pawn", "on board"],
+    ["wpawn.png", "null", "g2", "pawn", "on board"], ["wpawn.png", "null", "h2", "pawn", "on board"]];
+
     // Set the chessboard to a starting position
-    // Formatting = rows starting from bottom (1) to top (8)
     board = {
         1: [whitePieces[0], whitePieces[1], whitePieces[2], whitePieces[3], whitePieces[4], whitePieces[5], whitePieces[6], whitePieces[7]],
         2: [whitePawns[0], whitePawns[1], whitePawns[2], whitePawns[3], whitePawns[4], whitePawns[5], whitePawns[6], whitePawns[7]],
@@ -56,6 +49,8 @@ function resetBoard() {
         7: [blackPawns[0], blackPawns[1], blackPawns[2], blackPawns[3], blackPawns[4], blackPawns[5], blackPawns[6], blackPawns[7]],
         8: [blackPieces[0], blackPieces[1], blackPieces[2], blackPieces[3], blackPieces[4], blackPieces[5], blackPieces[6], blackPieces[7]]
     };
+    deletedPieces = 0;
+    updateBoard();
 }
 
 // Working
@@ -74,7 +69,7 @@ function updateBoard() {
                 <td id="g${row}"> <button id="${row}6" class="square"> <img src="img/${board[row][6][0]}"> </button> </td> 
                 <td id="h${row}" class="ds"> <button id="${row}7" class="square"> <img src="img/${board[row][7][0]}"> </button> </td> `)
         } else {
-            // Populate odd row with even odd coloring
+            // Populate odd row with odd square coloring
             $(`#${row}`).html(`<th>  ${row} </th> 
                 <td id="a${row}" class="ds"> <button id="${row}0" class="square"> <img src="img/${board[row][0][0]}"> </button> </td> 
                 <td id="b${row}"> <button id="${row}1" class="square"> <img src="img/${board[row][1][0]}"> </button> </td> 
