c4c766a3092de9d6f5dfcd04d2d26fab5fccd58e
Braden Rogers <brogers42@my.bcit.ca>
2023-05-11 11:01:09 -0700

    Profile writing to firestore

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -26,7 +26,7 @@ var uiConfig = {
           })
           .then(function () {
             console.log("New user added to firestore");
-            window.location.assign("placeholder.html"); //re-direct to main.html after signup
+            window.location.assign("board.html"); //re-direct to main.html after signup
           })
           .catch(function (error) {
             console.log("Error adding new user: " + error);
@@ -44,7 +44,7 @@ var uiConfig = {
   },
   // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
   signInFlow: "popup",
-  signInSuccessUrl: "placeholder.html",
+  signInSuccessUrl: "board.html",
   signInOptions: [
     // Leave the lines as is for the providers you want to offer your users.
     //   firebase.auth.GoogleAuthProvider.PROVIDER_ID,
