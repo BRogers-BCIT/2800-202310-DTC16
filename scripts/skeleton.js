eff2727d46cf1e581cff6b5365e4baa7249211ac
Sarah Ortega <ssjaneortega>
2023-05-11 09:43:39 -0700

    created js file to load navbars based on if user is logged in or not

---
+++ scripts/skeleton.js
@@ -0,0 +1,16 @@
+function loadSkeleton() {
+
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // User is signed in.
+            // Do something for the user here.
+            console.log($('#navbarPlaceholder').load('./bars/nav_after_login.html'));
+            console.log($('#footerPlaceholder').load('./bars/footer.html'));
+        } else {
+            // No user is signed in.
+            console.log($('#navbarPlaceholder').load('./bars/nav_b4_login.html'));
+            console.log($('#footerPlaceholder').load('./bars/footer.html'));
+        }
+    });
+}
+loadSkeleton(); //invoke the function
