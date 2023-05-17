4039926b52e889d48a8a02fd002309d83feca6e5
GurjeetB <gurjeetbhangoo@yahoo.com>
2023-05-17 10:17:15 -0700

    Add visual feedback for first step of easter egg

--- eggLink.js
+++ eggLink.js
@@ -1,11 +1,22 @@
+let eggStep = 0;
+
+function eggFeedback() {
+    console.log(eggStep);
+    if (eggStep >= 1) {
+        $(`header`).removeClass(`bg-secondary`).addClass(`bg-primary`);
+    } else {
+        $(`header`).removeClass(`bg-primary`).addClass(`bg-secondary`);
+    }
+}
+
 
 setup = function () {
-    let eggStep = 0;
+    eggStep = 0;
 
     $("#firstStep").click(function () {
         eggStep = 1;
         $("#thirdStep").html(`<a id="thirdStep" class="navbar-brand" href="#">.AI</a>`);
-        console.log(eggStep);
+        eggFeedback();
     });
     $("#secondStep").click(function () {
         if (eggStep == 1) {
@@ -15,14 +26,14 @@ setup = function () {
             eggStep = 0;
             $("#thirdStep").html(`<a id="thirdStep" class="navbar-brand" href="#">.AI</a>`);
         }
-        console.log(eggStep);
+        eggFeedback();
     });
     $("#thirdStep").click(function () {
         if (eggStep != 2) {
             eggStep = 0;
             $("#thirdStep").html(`<a id="thirdStep" class="navbar-brand" href="#">.AI</a>`);
         }
-        console.log(eggStep);
+        eggFeedback();
     });
 }
 $(document).ready(setup)
