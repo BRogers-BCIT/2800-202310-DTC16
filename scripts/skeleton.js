7a4a2f7cfda8a9482f362cad6d4d0bd368de5c56
GurjeetB <gurjeetbhangoo@yahoo.com>
2023-05-18 16:32:04 -0700

    Update console logging for deciding which navbar to use

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -8,17 +8,19 @@ function loadSkeleton() {
     firebase.auth().onAuthStateChanged(function (user) {
         if (path === indexPage) {
             // User is at the home page.
-            console.log(`User is at the home page.`)
+            console.log(`User is at home page`)
             console.log($('#navbarPlaceholder').load('../bars/nav_index.html'));
         } else { 
             // User is at a different page.
-            console.log(`User is at a different page.`)
+            console.log(`User is not at home page`)
             if (user) {
                 // User is signed in.
                 // Do something for the user here.
+                console.log(`Authentication successful`)
                 console.log($('#navbarPlaceholder').load('../bars/nav_after_login.html'));
             } else {
                 // No user is signed in.
+                console.log(`Authentication failed or missing`)
                 console.log($('#navbarPlaceholder').load('../bars/nav_b4_login.html'));
                 
             }
