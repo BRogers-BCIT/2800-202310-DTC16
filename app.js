552ca1131e8c4e6ff21e2216af82da2b1f1dc270
GurjeetB <gurjeetbhangoo@yahoo.com>
2023-05-22 15:48:48 -0700

    Add comments to all middleware

--- app.js
+++ app.js
@@ -5,53 +5,65 @@ const app = express();
 app.use(express.static('public')); // static files
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+// Loading default page
 app.get('/', (req, res) => {
     // TODO: Redirect user to /profile if they're already logged in
     res.sendFile(`${__dirname}/public/pages/index.html`);
 });
 
+// Loading analysis page normally
 app.get('/analysis', (req, res) => {
     res.sendFile(`${__dirname}/public/pages/analysis.html`);
 });
 
+// Sending FEN to analysis page
 app.post('/analysis', (req, res) => {
     console.log(`Got analysis request: ` + req.body.fenInput);
     // Arsam, do your thing here
     // Use req.body.fenInput to get what the user sent
-    // You may wanna protect against SQL attacks
     res.sendFile(`${__dirname}/public/pages/analysis.html`);
 });
 
+// Loading board editor page normally
 app.get('/board', (req, res) => {
     res.sendFile(`${__dirname}/public/pages/board.html`);
 });
 
+// Loading easter egg
 app.get('/egg', (req, res) => {
     res.sendFile(`${__dirname}/public/pages/egg.html`);
 });
 
+// Loading index page
 app.get('/index', (req, res) => {
     res.sendFile(`${__dirname}/public/pages/index.html`);
 });
 
+// Loading login page
 app.get('/login', (req, res) => {
     res.sendFile(`${__dirname}/public/pages/login.html`);
 });
 
+// Loading board editor with a board already opened
 app.get('/openBoard', (req, res) => {
     res.sendFile(`${__dirname}/public/pages/openBoard.html`);
 });
 
+// Loading profile page
 app.get('/profile', (req, res) => {
     res.sendFile(`${__dirname}/public/pages/profile.html`);
 });
 
+// Loading saved boards page
 app.get('/saved', (req, res) => {
     res.sendFile(`${__dirname}/public/pages/saved.html`);
 });
 
+// Error 404
 app.get('*', (req, res) => {
     res.sendFile(`${__dirname}/public/pages/404.html`);
 });
 
+// Starting server
 module.exports = app;
