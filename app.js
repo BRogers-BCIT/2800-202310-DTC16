7aa07b1f395ffa5117f4a2cb483cbce5f52f0d38
GurjeetB <gurjeetbhangoo@yahoo.com>
2023-05-22 14:30:13 -0700

    Replace file calls in HTML hrefs with paths

--- app.js
+++ app.js
@@ -7,6 +7,38 @@ app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/public/pages/index.html`);
 });
 
+app.get('/analysis', (req, res) => {
+    res.sendFile(`${__dirname}/public/pages/analysis.html`);
+});
+
+app.get('/board', (req, res) => {
+    res.sendFile(`${__dirname}/public/pages/board.html`);
+});
+
+app.get('/egg', (req, res) => {
+    res.sendFile(`${__dirname}/public/pages/egg.html`);
+});
+
+app.get('/index', (req, res) => {
+    res.sendFile(`${__dirname}/public/pages/index.html`);
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(`${__dirname}/public/pages/login.html`);
+});
+
+app.get('/openBoard', (req, res) => {
+    res.sendFile(`${__dirname}/public/pages/openBoard.html`);
+});
+
+app.get('/profile', (req, res) => {
+    res.sendFile(`${__dirname}/public/pages/profile.html`);
+});
+
+app.get('/saved', (req, res) => {
+    res.sendFile(`${__dirname}/public/pages/saved.html`);
+});
+
 app.get('*', (req, res) => {
     res.send('404 Not Found');
 });
