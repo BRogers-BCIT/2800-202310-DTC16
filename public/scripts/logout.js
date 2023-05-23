312e2ec7a6685b447ba4ee0f082b62ab627d48aa
GurjeetB <gurjeetbhangoo@yahoo.com>
2023-05-23 14:10:10 -0700

    Clear local storage upon logging out (How the fuck did I miss this)

--- public/scripts/logout.js
+++ public/scripts/logout.js
@@ -2,6 +2,7 @@ function logout() {
     firebase.auth().signOut().then(() => {
         // Sign-out successful.
         console.log("logging out user");
+        localStorage.clear();
       }).catch((error) => {
         // An error happened.
       });
