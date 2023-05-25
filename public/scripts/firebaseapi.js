15c005a1528703cb7c13032f8a343a28873e6b9b
Sarah Ortega <ssjaneortega>
2023-05-25 11:19:06 -0700

    commented on firebaseapi.js

--- public/scripts/firebaseapi.js
+++ public/scripts/firebaseapi.js
@@ -1,3 +1,5 @@
+// these are the firebase api keys to connect to the firebase database
+
 const firebaseConfig = {
     apiKey: "AIzaSyDKh578MFnYCcNWOjEvO0pLz8I76oMKU7g",
     authDomain: "dtc-16-comp2800.firebaseapp.com",
@@ -7,6 +9,8 @@ const firebaseConfig = {
     appId: "1:999235547829:web:7c7c1b4b5f03ffb7ab84fc",
     measurementId: "G-RTXNFE170Q"
   };
+
+// Initialize Firebase
 
 const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
