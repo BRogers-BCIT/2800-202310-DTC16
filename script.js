f86af50c6b6c2748afdaba8ae839c7620c18cee3
Braden Rogers <brogers42@my.bcit.ca>
2023-05-25 13:05:04 -0700

    Clean And Commented By Arsam On Braden's Device

--- script.js
+++ script.js
@@ -1,34 +1,44 @@
+// Import text Configuration
 import { config } from "dotenv"
 config()
 
+// Import API
 import { Configuration, OpenAIApi } from "openai"
+
+// Import Text Reader
 import readline from "readline"
 
+// Fetches FEN from firebase
 var currentFEN = "temp"
-        db.collection("users").doc(uUid).get("currentFEN").then(function (doc) {
-            currentFEN = doc.data().currentFEN;
-        }).then(function () {
-            
-            const openai =new OpenAIApi (new Configuration({
-                apiKey: process.env.API_KEY
-            }))
-            
-            const userInterface = readline.createInterface({
-                input: process.stdin,
-                output: process.stdout
-            })
-            
-            userInterface.prompt()
-            userInterface.on("line", async input => {
-                const res = await openai.createChatCompletion({
-                    model: "gpt-3.5-turbo",
-                    messages: [{role: "user", content: currentFEN}]
-                })
-                console.log(res.data.choices[0].message.content)
-                userInterface.prompt()
-            })
 
+// Call User Database
+db.collection("users").doc(uUid).get("currentFEN").then(function (doc) {
+    currentFEN = doc.data().currentFEN;
+}).then(function () {
+
+    // Accesses API
+    const openai = new OpenAIApi(new Configuration({
+        apiKey: process.env.API_KEY
+    }))
+
+    // Create user interface
+    const userInterface = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    })
+
+    // Prompt user
+    userInterface.prompt()
+    userInterface.on("line", async input => {
+        const res = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [{ role: "user", content: currentFEN }]
         })
+        console.log(res.data.choices[0].message.content)
+        userInterface.prompt()
+    })
+
+})
 
 
 
